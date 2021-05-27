adf8cac6ef4060909f5ec933a34ea8a0892a9138
Thais Goncalves da Silva <thaisgs120@gmail.com>
2021-05-27 02:33:34 -0300

    Limpeza do código

--- anotacao.c
+++ anotacao.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "menu.h"
+#include <termios.h>
+#include <unistd.h>
+#include "anotacao.h"
+
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
 
 struct no
 {
